f384d16f44a42f84a5533cffc7c735e320bc4d09
IgorStatkevitch <71011941+IgorStatkevitch@users.noreply.github.com>
2020-11-27 00:07:07 +0300

    Update sum.js

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,5 @@
 function sum(a, b) {
-  if (typeof a !== "number" || typeof a !== "number") {
+  if (typeof a !== "number" || typeof b !== "number") {
     throw new TypeError();
   }
   return a + b;
