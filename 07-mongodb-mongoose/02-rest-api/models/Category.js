03500924238d4c576aebda2bd006dc72fd445d16
Igor Statkevich <igorblood906@gmail.com>
2020-12-23 15:53:58 +0300

    feat: add solution for 07-mongodb-mongoose 02-rest-api

--- 07-mongodb-mongoose/02-rest-api/models/Category.js
+++ 07-mongodb-mongoose/02-rest-api/models/Category.js
@@ -1,20 +1,44 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
-const subCategorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const subCategorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
 
-const categorySchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const categorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    subcategories: [subCategorySchema],
   },
-
-  subcategories: [subCategorySchema],
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
 
 module.exports = connection.model('Category', categorySchema);
--- 07-mongodb-mongoose/02-rest-api/models/Product.js
+++ 07-mongodb-mongoose/02-rest-api/models/Product.js
@@ -1,35 +1,46 @@
 const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
-const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+
+    subcategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+
+    images: [String],
   },
-
-  description: {
-    type: String,
-    required: true,
-  },
-
-  price: {
-    type: Number,
-    required: true,
-  },
-
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category',
-    required: true,
-  },
-
-  subcategory: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-
-  images: [String],
-
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
 
 module.exports = connection.model('Product', productSchema);
