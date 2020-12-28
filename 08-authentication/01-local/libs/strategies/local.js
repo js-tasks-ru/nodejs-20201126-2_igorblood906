89e34b09520f78b7f1594275652813d3616a2cd5
Igor Statkevich <igorblood906@gmail.com>
2020-12-28 17:09:38 +0300

    feat:add solution for 08-authentication 01-local

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,20 @@
 const LocalStrategy = require('passport-local').Strategy;
 
 module.exports = new LocalStrategy(
-    {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
-    },
+  { usernameField: 'email', session: false },
+  async function (email, password, done) {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error('Нет такого пользователя');
+      }
+      const correctPass = await user.checkPassword(password);
+      if (!correctPass) {
+        throw new Error('Неверный пароль');
+      }
+      done(null, user);
+    } catch (error) {
+      done(null, false, error.message);
+    }
+  }
 );
